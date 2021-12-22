9fa486bf8c36a51015ac9cdd17d442a7720f3ed4
kuifje0257 <kuifje0257@hotmail.be>
2021-12-22 14:16:00 +0100

    Added msleep()

--- kernel_ZP.c
+++ kernel_ZP.c
@@ -3,6 +3,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
+#include <linux/delay.h>
 
 /*
  * Struct defining pins, direction and inital state 
@@ -19,17 +20,24 @@ static struct gpio leds[] = {
 static int __init gpiomod_init(void)
 {
 	int ret = 0;
+	int i;
 
 	printk(KERN_INFO "%s\n", __func__);
+	
 
 
 	// register LED GPIOs, turn LEDs on
 	ret = gpio_request_array(leds, ARRAY_SIZE(leds));
 
+	for(i = 0; i < ARRAY_SIZE(leds); i++) {
+		gpio_set_value(leds[i].gpio, 1); 
+		printk(KERN_INFO "BMC: %d is aan.",leds[i].gpio);
+	}
+
 	if (ret) {
 		printk(KERN_ERR "Unable to request GPIOs: %d\n", ret);
 	}
-
+	msleep(1);
 	return ret;
 }
 
@@ -50,10 +58,12 @@ static void __exit gpiomod_exit(void)
 	
 	// unregister all GPIOs
 	gpio_free_array(leds, ARRAY_SIZE(leds));
+	msleep(1);
 }
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Branko De Caluwé");
 MODULE_DESCRIPTION("Opdracht6-Kernel");
 
 module_init(gpiomod_init);
+
 module_exit(gpiomod_exit);
